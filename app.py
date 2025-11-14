c877adb96ad69bdc89447f234ee716d1458dcc8d
meron_daniel <dmeron29@gmail.com>
2025-11-14 11:05:15 -0500

    app.py adds post, put and delete method and test_app tests those methods being successful

--- app.py
+++ app.py
@@ -1,11 +1,44 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+app.echos = {}
 
 
 @app.route('/hello', methods=['GET'])
 def hello():
     return jsonify(message="Hello, World!")
+
+
+@app.route('/echo', methods=['POST'])
+def echo():
+    data = request.get_json(force=True)
+    return jsonify(data), 201
+
+
+@app.route('/echo/<int:echo_id>', methods=['PUT'])
+def echo_put(echo_id):
+    data = request.get_json(force=True)
+    msg = data.get('msg')
+
+    updated_echo = {'id': echo_id, 'msg': msg}
+
+    return jsonify(
+        updated_echo
+    ), 201
+
+
+@app.route('/echo/<int:echo_id>', methods=['DELETE'])
+def echo_delete(echo_id):
+
+    if echo_id not in app.echos:
+        return jsonify({'error': 'Task not found'}), 404
+
+    del app.echos[echo_id]
+
+    return jsonify({
+        "message": "Echo has been deleted!"
+    }), 200
 
 
 if __name__ == '__main__':
--- test_app.py
+++ test_app.py
@@ -1,8 +1,37 @@
-import app
+from flask import json
+from app import app
 
 
 def test_hello():
-    client = app.app.test_client()
+    client = app.test_client()
     response = client.get('/hello')
     assert response.status_code == 200
     assert response.get_json() == {"message": "Hello, World!"}
+
+def test_echo():
+    client = app.test_client()
+    payload = {"msg": "ping"}
+    response = client.post('/echo', json=payload)
+    assert response.status_code == 201
+    assert response.get_json() == payload
+
+def test_echo_put():
+    client = app.test_client()
+    echo_id = 1
+    payload = {
+        "id": echo_id,
+        "msg": "hello, this is a message I want to test"}
+    response = client.put(f"/echo/{echo_id}", json=payload)
+    assert response.status_code == 201
+    assert response.get_json() == payload
+
+def test_echo_delete():
+    client = app.test_client()
+    echo_id = 1
+
+    # Arrange: add an echo
+    app.echos[echo_id] = {"id": echo_id, "msg": "test message"}
+
+    response = client.delete(f"/echo/{echo_id}")
+    assert response.status_code == 200
+    assert response.get_json() == {"message": "Echo has been deleted!"}
